c589589548b3757a3c4f635b1079ccfbbdc4022c
Anirudh Jwala <anirudhsai2@gmail.com>
2023-05-09 22:40:09 +0530

    first commit

---
+++ Day01/LeetCode2667.js
@@ -0,0 +1,7 @@
+/**
+ * Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+ */
+
+var createHelloWorld = function () {
+  return () => 'Hello World';
+};
