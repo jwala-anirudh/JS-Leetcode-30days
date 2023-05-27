aaef46c205d59419f2b292b6a5f774a5e4d66604
Anirudh Jwala <anirudhsai2@gmail.com>
2023-05-27 08:14:37 +0530

    Leetcode daily challenge Javascript

---
+++ Day23/LeetCode2619.js
@@ -0,0 +1,17 @@
+/**
+ * Array Prototype Last
+ *
+ *
+ * Write code that enhances all arrays such that you can call the array.last()
+ * method on any array and it will return the last element.
+ * If there are no elements in the array, it should return -1.
+ */
+Array.prototype.last = function () {
+  if (this.length > 0) {
+    return this[this.length - 1];
+  }
+  return -1;
+};
+
+const arr = [1, 2, 3];
+arr.last(); // 3
